b371e35014d8a753542a57896273157bfa0f6c53
percyF <percy8ferry@gmail.com>
2018-08-01 17:29:05 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// import filesystem library
+const fs = require('fs');
+// import csvtojson library
+const toJson = require('csvtojson');
+
+//create path to input file
+const inputPath='./data/customer-data.csv';
+
+//create path to output file
+const outputPath='./data/JSON.json';
+
+// use csvtojson to process the input file
+toJson().fromFile(inputPath).then((jsonObj)=>{
+  // make sure to "stringify"nthe json object befire saving
+    fs.writeFile( outputPath, JSON.stringify(jsonObj), function (err) {
+      // check for errors
+      if (err) return console.error("An error of type { " + error + " } has occured.");
+      // confirm that the job is done and file was written
+      console.log('CSV converted to JSON and written to file.');
+    });
+});
