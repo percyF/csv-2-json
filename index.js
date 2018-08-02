0dd0430e259c8be86470d5b98019e9685bc2e883
percyF <percy8ferry@gmail.com>
2018-08-02 10:45:56 +1000

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const outputPath='./data/JSON.json';
 
 // use csvtojson to process the input file
 toJson().fromFile(inputPath).on('error', (err)=>{
-  // we have an error, log the error and then exit()
+    // we have an error, log the error and then exit()
     console.log(err);
     process.exit();
   }).then((jsonObj)=>{
-  // make sure to "stringify" the json object before saving
+  
+  // make sure to "stringify" the json object befire saving
     fs.writeFile( outputPath, JSON.stringify(jsonObj, null, 2), function (err) {
       // check for errors
       if (err) return console.error("An error of type { " + error + " } has occured.");
